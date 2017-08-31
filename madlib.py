ea942ff5b76ea52a3c4ad6ee907095e5e6ccfaac
LoftTimber <31486781+LoftTimber@users.noreply.github.com>
2017-08-31 14:52:19 -0400

    Add files via upload

--- madlib.py
+++ madlib.py
@@ -9,56 +9,45 @@ print("Welcome to Mad Libs!")
 print("What is your name?")
 name=input()
 
-print("Insert an adjective.")
-adj1=input()
-
 print("Insert a verb.")
 v1=input()
 
-print("Insert another verb.")
-v2=input()
+print("Insert two adjectives.")
+adj1=input()
+adj2=input()
 
 print("Insert a relation. Ex: friend, teacher, mother, ...")
 person1=input()
 
-print("Insert a fruit.")
-fruit1=input()
+print("Insert a number.")
+number1=input()
 
 print("Insert a season.")
 season1=input()
 
-print("Insert a person's name.")
+print("Insert three people's name.")
 person2=input()
-
-print("Insert another person's name.")
 person3=input()
-
-print("Insert a place.")
-place=input()
-
-print("Insert a person's name.")
 person4=input()
 
-print("Insert another person's name.")
-person5=input()
-
-print("Insert a verb.")
-v3=input()
+print("Insert the name of a business.")
+place=input()
 
 print("Insert a color.")
-color1=input
+color1=input()
 
-print("Insert an adjective.")
-adj2=input
+print("Insert a food.")
+food1=input()
+
 
 print ("Red, "+adj1+" apples!")
 print ("Today we are going to "+v1+" apples.")
-print ("I am going to "+v2+" the most.")
-print ("My (person1) and I are having an (fruit1) picking contest this year.")
-print ("Every (season1) we go to (person's2) farm to pick a (n1) of apples.")
-print ("This year (person3) wants to make (n2), so we needa lot.")
-print ("When we arrive (place1), (person4) counts out our apples.")
+print ("I am going to "+v1+" the most.")
+print ("My "+person1+" and I are having an apple "+v1+" contest this year.")
+print ("Every "+season1+" we go to "+person2+"'s farm to pick "+number1+" apples.")
+print ("This year "+person3+" wants to make "+food1+", so we need alot.")
+print ("When we arrive at "+place1+", "+person4+" counts out our apples.")
 print ("We anxiously await the final count.")
-print ("My (person5) and I (v3)ed!")
-print ("Well actually I had one more then him, but it had a (color1) slimy worm in it.")
-print ("That night we had (adj2) applesauce!")
+print ("My "+person1+" and I tied!")
+print ("Well actually I had one more then him, but it had a "+color1+" worm in it.")
+print ("That night we had "+adj2+" applesauce!")
