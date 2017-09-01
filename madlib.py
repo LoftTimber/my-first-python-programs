f5df6945fc34d94f72c6ff893a51f7e4112b874f
LoftTimber <31486781+LoftTimber@users.noreply.github.com>
2017-09-01 14:45:06 -0400

    Add files via upload

--- madlib.py
+++ madlib.py
@@ -5,7 +5,7 @@
 
 
 print("Welcome to Mad Libs!")
-
+print ()
 print("What is your name?")
 name=input()
 
@@ -31,7 +31,7 @@ person3=input()
 person4=input()
 
 print("Insert the name of a business.")
-place=input()
+place1=input()
 
 print("Insert a color.")
 color1=input()
@@ -39,15 +39,17 @@ color1=input()
 print("Insert a food.")
 food1=input()
 
-
+print ("A Trip to The Apple Orchard")
+print ("By:"+name)
+print ()
 print ("Red, "+adj1+" apples!")
 print ("Today we are going to "+v1+" apples.")
 print ("I am going to "+v1+" the most.")
-print ("My "+person1+" and I are having an apple "+v1+" contest this year.")
+print ("My "+person1+" and I are having an apple "+v1+"ing contest this year.")
 print ("Every "+season1+" we go to "+person2+"'s farm to pick "+number1+" apples.")
-print ("This year "+person3+" wants to make "+food1+", so we need alot.")
+print ("This year "+person3+" wants to make "+food1+", so we need alot of apples.")
 print ("When we arrive at "+place1+", "+person4+" counts out our apples.")
 print ("We anxiously await the final count.")
 print ("My "+person1+" and I tied!")
-print ("Well actually I had one more then him, but it had a "+color1+" worm in it.")
+print ("Well, actually I had one more apple, but it had a "+color1+" worm in it.")
 print ("That night we had "+adj2+" applesauce!")
